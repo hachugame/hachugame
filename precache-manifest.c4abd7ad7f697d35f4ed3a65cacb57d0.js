7a26d9801ad375419aa8e87ad3ee1a65f90275a6
hachugame <50293688+hachugame@users.noreply.github.com>
2019-05-05 13:58:01 +0100

    Updates

---
+++ precache-manifest.c4abd7ad7f697d35f4ed3a65cacb57d0.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "78a5520f67f4cdc661b86b9f398dddc9",
+    "url": "/hachugame/index.html"
+  },
+  {
+    "revision": "f0f20a441791af570774",
+    "url": "/hachugame/static/css/main.584f321a.chunk.css"
+  },
+  {
+    "revision": "b633ec75ef052e45c84b",
+    "url": "/hachugame/static/js/2.886325e0.chunk.js"
+  },
+  {
+    "revision": "f0f20a441791af570774",
+    "url": "/hachugame/static/js/main.b779af18.chunk.js"
+  },
+  {
+    "revision": "abfe4160ab74dcd31c64",
+    "url": "/hachugame/static/js/runtime~main.659efdeb.js"
+  },
+  {
+    "revision": "2c330888af99dd729e101201121d3f91",
+    "url": "/hachugame/static/media/hachu.2c330888.png"
+  }
+]);
